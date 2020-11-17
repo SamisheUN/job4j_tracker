8d5e23a3a6c44f23450778d94a40ddb26accf078
Andrey Solarsky <SerSam-2007@yandex.ru>
2020-11-17 13:47:44 +0400

    1.5. Поля объекта [#166083] Создание поля имени кота и вывод на консоль

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,32 @@
-package ru.job4j.oop; //hmm
+package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void show() { //метод вывода еды
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void giveNick (String nick) {
+        this.name = nick;
+    }
+
+    public void eat (String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        System.out.println("There are gav's name an food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Gav");
+        gav.show();
+        System.out.println("There are black's name an food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Black");
+        black.show();
     }
 }
