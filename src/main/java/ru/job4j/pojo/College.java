e0888cf329824dac386c59376d29218d87660c6a
Andrey Solarsky <SerSam-2007@yandex.ru>
2020-12-21 11:51:30 +0400

    2. Модель данных. [#432633]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.setDate("03.11.2018");
+        student.setFullname("Andrey Samoylychev");
+        student.setGroup("trainee");
+
+        System.out.println(student.getFullname() + " " +
+                student.getGroup() + " since " + student.getDate());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String fullname;
+    private String group;
+    private String date;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
