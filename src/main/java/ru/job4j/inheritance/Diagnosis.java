c7eded78652fa415b566798cf917e6495036eca1
Andrey Solarsky <SerSam-2007@yandex.ru>
2020-12-27 21:03:18 +0400

    1. Чтение из консоли. Класс Scanner. [#432562]

--- src/main/java/ru/job4j/inheritance/Diagnosis.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.inheritance;
-
-public class Diagnosis extends Doctor{
-
-    public String analyze() {
-        return null;
-    }
-}
--- src/main/java/ru/job4j/inheritance/Pacient.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.inheritance;
-
-public class Pacient extends Doctor{
-    private String pacientname;
-    private double pacientage;
-    private double pacientweight;
-}
