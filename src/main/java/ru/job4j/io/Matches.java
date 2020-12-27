201b934edc2910195305091eed987a183194594e
Andrey Solarsky <SerSam-2007@yandex.ru>
2020-12-28 00:08:09 +0400

    2. Scanner и чтение числа из консоли. [#432563]

---
+++ src/main/java/ru/job4j/io/Matches.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int pool = 11;
+        boolean first = false;
+        int player = new Random().nextInt(1); //жребий первого хода 1/0
+        while (pool > 0) {
+            System.out.println("На столе " + pool + " спичек.");
+            System.out.println("Число от игрока " + player + ": ");
+            int select = Integer.valueOf(input.nextLine());
+            if (select < 4 && select > 0) {          //проверка вводного числа
+                pool = pool - select;
+                player = player == 1 ? 0 : 1;
+            } else {
+                System.out.println("Некорректный ввод");
+            }
+        }
+        player = player == 1 ? 0 : 1;
+        System.out.println("Победитель: Игрок " + player);
+    }
+}
