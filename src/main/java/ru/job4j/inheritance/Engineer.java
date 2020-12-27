e11ff63970f47439dedad3277b3a2341b451d03e
Andrey Solarsky <SerSam-2007@yandex.ru>
2020-12-27 21:04:00 +0400

    Revert "1. Чтение из консоли. Класс Scanner. [#432562]"
    This reverts commit c7eded78

---
+++ src/main/java/ru/job4j/inheritance/Engineer.java
@@ -0,0 +1,13 @@
+package ru.job4j.inheritance;
+
+public class Engineer {
+    private int teammates;
+
+    public Project work(Project project) {
+        return null;
+    }
+
+    public int getTeammates(){
+     return 0;
+    }
+}
