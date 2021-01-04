76c53287dd2a441fac4c4a480ee8bb6542257065
Andrey Solarsky <SerSam-2007@yandex.ru>
2021-01-04 19:54:25 +0400

    3. Интерфейс Input [#432564]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void riding() {
+
+    }
+
+    @Override
+    public void passengers(int people) {
+
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void riding();
+
+    void passengers(int people);
+
+    double refuel(double fuel);
+}
