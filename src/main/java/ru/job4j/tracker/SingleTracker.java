9a4eb4c41b4cb55abf308191bd338f082dd60d47
Andrey Solarsky <SerSam-2007@yandex.ru>
2021-03-06 11:54:31 +0400

    2. Singleton. [#432598]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -2,7 +2,9 @@ package ru.job4j.tracker;
 
 public class SingleTracker {
     private Tracker tracker = new Tracker();
-    private static SingleTracker instance = null;
+    private static SingleTracker instance;
+    private SingleTracker() {
+    }
 
     public static SingleTracker getInstance() {
         if (instance == null) {
