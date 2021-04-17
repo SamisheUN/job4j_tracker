6ae6fb070b893af1e65197a2da3d4daf6d19e429
Andrey Solarsky <SerSam-2007@yandex.ru>
2021-04-18 03:47:43 +0400

    1. Организовать сортировку User [#432537]

---
+++ src/test/java/ru/job4j/collection/UserTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import ru.job4j.bank.User;
+import ru.job4j.tracker.Item;
+import ru.job4j.tracker.SortItemIncrOrder;
+
+import java.util.*;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UserTest {
+    @Test
+    public void whenAsc() {
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Z", 9000));
+        users.add(new User("Petr", 32));
+        users.add(new User("Z", 9001));
+        users.add(new User("Ivan", 31));
+        Iterator<User> it = users.iterator();
+        assertThat(it.next(), is(new User("Ivan", 31)));
+        assertThat(it.next(), is(new User("Petr", 32)));
+        assertThat(it.next(), is(new User("Z", 9000)));
+        assertThat(it.next(), is(new User("Z", 9001)));
+    }
+
+    @Test
+    public void whenComparePertVSIvan() {
+        int rsl = new User("Petr", 32)
+                .compareTo(
+                        new User("Ivan", 31)
+                );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenCompareOneNameDiffAge() {
+        int rsl = new User("A", 1)
+                .compareTo(
+                        new User("A", 2)
+                );
+        assertThat(rsl, is(-1));
+    }
+
+    @Test
+    public void whenCompareOneNameOneAge() {
+        int rsl = new User("A", 1)
+                .compareTo(
+                        new User("A", 1)
+                );
+        assertThat(rsl, is(0));
+    }
+}
