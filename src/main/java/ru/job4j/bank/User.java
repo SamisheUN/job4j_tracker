b108890d9ea0b12ba2058bdbcdb46a7651a1e1d5
Andrey Solarsky <SerSam-2007@yandex.ru>
2021-04-18 03:45:56 +0400

    1. Организовать сортировку User [#432537]

--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -1,13 +1,22 @@
 package ru.job4j.bank;
 
+import ru.job4j.tracker.Item;
+
+import java.util.Comparator;
 import java.util.Objects;
 
-public class User {
+public class User implements Comparable<User>, Comparator<User> {
     private String passport;
     private String username;
+    private int age;
 
     public User(String passport, String username) {
         this.passport = passport;
+        this.username = username;
+    }
+
+    public User(String username, int age) {
+        this.age = age;
         this.username = username;
     }
 
@@ -27,16 +36,53 @@ public class User {
         this.username = username;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+    /* @Override //старый по паспорту
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return Objects.equals(passport, user.passport);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(passport);
+        }*/
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(passport, user.passport);
+        return age == user.age &&
+                Objects.equals(username, user.username);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(passport);
+        return Objects.hash(username, age);
+    }
+
+    @Override
+    public int compareTo(User another) {
+        int rsl = CharSequence.compare(username, another.getUsername());
+        if (rsl == 0) {
+            rsl = Integer.compare(age, another.getAge());
+        }
+        return rsl;
+    }
+
+    @Override
+    public int compare(User o1, User o2) {
+        return o1.compareTo(o2);
     }
 }
