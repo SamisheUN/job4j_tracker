e04f38655ee1953320093b0b20e1fdc34cf1c53f
Andrey Solarsky <SerSam-2007@yandex.ru>
2021-04-22 04:04:50 +0400

    4. Сортировка номера [#432536]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -3,20 +3,11 @@ package ru.job4j.collection;
 import java.util.Comparator;
 
 public class LexSort implements Comparator<String> {
-    public int digitsextractor(String word) {
-        String digit = new String();
-        for (int index = 0; index < word.length(); index++) {
-            if (Character.isDigit((word.charAt(index)))) {
-                digit = digit + word.charAt(index);
-            } else {
-                break;
-            }
-        }
-        return Integer.parseInt(digit);
-    }
 
     @Override
     public int compare(String left, String right) {
-        return Integer.compare(digitsextractor(left), digitsextractor(right));
+        return Integer.compare(
+                Integer.parseInt(left.split("\\.")[0]),
+                Integer.parseInt(right.split("\\.")[0]));
     }
 }
